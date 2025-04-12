25fbc3297948768dde31331b5ecdc04a5957241e
Anedtech <anedtech@gmail.com>
2025-04-11 21:48:15 -0700

    todays update

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:locality_chat/login_page.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
@@ -28,12 +29,12 @@ class RegisterPage extends StatelessWidget {
           ),
           SizedBox(height: 92),
           Padding(
-            padding: const EdgeInsets.only(left: 24, right: 24),
+            padding: const EdgeInsets.only(right: 24, left: 24),
             child: TextField(
               decoration: InputDecoration(
-                hintText: 'Enter Email',
+                hintText: 'Enter your name',
                 hintStyle: TextStyle(color: Colors.white),
-                suffixIcon: Icon(Icons.email, color: Colors.white),
+                suffixIcon: Icon(Icons.person, color: Colors.white),
               ),
             ),
           ),
@@ -47,6 +48,75 @@ class RegisterPage extends StatelessWidget {
                 suffixIcon: Icon(Icons.password, color: Colors.white),
               ),
             ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24, left: 24),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Comfirm Password',
+                hintStyle: TextStyle(color: Colors.white),
+                suffixIcon: Icon(Icons.password, color: Colors.white),
+              ),
+            ),
+          ),
+          SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Container(
+                  width: 300,
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    gradient: LinearGradient(
+                      colors: [Color(0xFFFF7E5F), Color(0xFFFEB47B)],
+                    ),
+                  ),
+                  child: Text(
+                    'Register Now!',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Already have an account',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(width: 5),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
